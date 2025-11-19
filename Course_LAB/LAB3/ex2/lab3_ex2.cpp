955cb7c752b322afa3720af9fda1df885a0f89cd
WxxGreat <1124943529@qq.com>
2025-11-19 21:58:25 +0800

    [ADD] add ex2 code

---
+++ Course_LAB/LAB3/ex2/lab3_ex2.cpp
@@ -0,0 +1,100 @@
+#pragma optimize( "", off) // Disable optimizations for this file
+#include "tpl_os.h"
+#include "Arduino.h"
+#include <stdlib.h>
+
+DeclareResource(TaskA_C_Resource);
+
+void MY_serial_print(unsigned long start, unsigned long end, unsigned long* max_resp_C);
+
+#define ON  1
+#define OFF 0
+#define LED1(state) digitalWrite(5, (state == ON) ? OFF : ON)
+#define LED2(state) digitalWrite(6, (state == ON) ? OFF : ON)
+#define LED3(state) digitalWrite(7, (state == ON) ? OFF : ON)
+
+void do_things(int ms)
+{
+    unsigned long mul = ms * 504UL;
+    for (unsigned long i = 0; i < mul; i++)
+    {
+        millis();
+    }
+}
+
+void setup()
+{
+	pinMode(5, OUTPUT);
+    pinMode(6, OUTPUT);
+    pinMode(7, OUTPUT);
+    LED1(OFF);
+    LED2(OFF);
+    LED3(OFF);
+    Serial.begin(115200);
+}
+
+TASK(TaskA)
+{
+    LED1(ON);
+    GetResource(TaskA_C_Resource);
+    do_things(200);
+    ReleaseResource(TaskA_C_Resource);
+    LED1(OFF);
+    TerminateTask();
+}
+
+TASK(TaskB) 
+{
+    LED2(ON);
+    do_things(700);
+    LED2(OFF);
+    TerminateTask();
+}
+
+
+TASK(TaskC)
+{
+    static unsigned long max_resp_C = 0;
+    unsigned long start = 0, end = 0;;
+    LED3(ON);
+    start = millis();
+
+    do_things(100);
+    GetResource(TaskA_C_Resource);
+    do_things(200);
+    ReleaseResource(TaskA_C_Resource);
+
+    end = millis();
+    MY_serial_print(start, end, &max_resp_C);
+
+    LED3(OFF);
+    TerminateTask();
+}
+    
+void MY_serial_print(unsigned long start, unsigned long end, unsigned long* max_resp_C)
+{
+    unsigned long resp = 0;
+    static unsigned long activation_count = 0;
+
+    activation_count++;
+    resp = end - start;
+
+    if (resp > *max_resp_C)
+    {
+        *max_resp_C = resp;
+        Serial.print("[TaskC] New max response: ");
+        Serial.print(*max_resp_C);
+        Serial.println(" ms");
+    }
+
+    /* Print the last activation times and response for tracing */
+    Serial.print("[TaskC] #");
+    Serial.print(activation_count);
+    Serial.print(" start=");
+    Serial.print(start);
+    Serial.print(" ms, end=");
+    Serial.print(end);
+    Serial.print(" ms, resp=");
+    Serial.print(resp);
+    Serial.println(" ms");
+}
